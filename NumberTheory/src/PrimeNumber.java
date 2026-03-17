fb9ee54dd121199e7d3f725455d4e0e43e534d27
Saiyed Samima <saiyedsamima20@gmail.com>
2026-03-17 12:45:40 +0530

    Number Thoery

---
+++ NumberTheory/src/PrimeNumber.java
@@ -0,0 +1,17 @@
+public class PrimeNumber {
+    static boolean isPrime(int n){
+        if(n <= 1){
+            return false;
+        }
+        for(int i=2; i*i<=n;i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPrime(5));
+        System.out.println(isPrime(25));
+    }
+}
